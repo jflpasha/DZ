5296fc7d7329b6dbc8951b70f43c58cc3f2e4621
PavelShi <jflpasha@mail.ru>
2022-12-06 16:50:01 +0300

    Домашняя работа 2го семинара

---
+++ Sem2Dz1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введи трёхзначное число: ");   //выводи текст
+
+// Преобразуем в масив
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine()); 
+string stringNumber = Convert.ToString(threeDigitNumber);
+//выводим второе число масива
+Console.WriteLine("вторая цифра этого числа --> "+stringNumber[1]);
---
+++ Sem2Dz2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Вывод текста
+Console.Write("Введи число: ");
+//преобразование в массив
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+//В преобразованном массиве смотрим что бы он был больше 2х 
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);  // Выводим 2е число массива
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");   
+}
