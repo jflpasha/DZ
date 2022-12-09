247652f9e079473f5f6dae3e6bbf5141ea801617
PavelShi <jflpasha@mail.ru>
2022-12-09 18:16:08 +0300

    ДЗ к семенару 3

---
+++ Sem3Dz1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]) // сравниваем 1-5 и 2-4 числа
+  {
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
+// Проверка на то что число 5и значное
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число"); 
---
+++ Sem3Dz3/Program.cs
@@ -0,0 +1,31 @@
+﻿ int number = ReadData("Введите число: ");
+ string outLine = LineBuilder(number, 1 );
+ PrintData("", outLine);
+ outLine = LineBuilder(number, 3 );     // ПРАВКА ДЛЯ ВОЗВЕДЕНИЯ В КУБ =)
+ PrintData("", outLine);
+
+
+ // Метод вывода данных
+ void PrintData( string msg, string res)
+ {
+    Console.WriteLine(msg + res);
+ }
+
+ // Метод сборки строки
+ string LineBuilder(int n, int pow)
+ {
+     string line = string.Empty;
+     for (int i=1; i<n; i++)
+     {
+         line = line + Math.Pow (i, pow) + " ";
+     }
+     line = line + Math.Pow (n, pow);
+     return line;
+ }
+
+ // Метод читает данные от пользователя
+ int ReadData(string msg)
+ {
+     Console.Write(msg);
+     return int.Parse(Console.ReadLine() ?? "0");
+ }
